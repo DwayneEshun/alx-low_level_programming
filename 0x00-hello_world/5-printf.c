fb2fdbbf92a2c8f67da349c80f43d6667eeee3a0
DwayneEshun <dwayneeshun@gmail.com>
2023-02-16 22:18:12 +0000

    The art of Grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main is the real function
+ *
+ * Return: Gives you 0
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
