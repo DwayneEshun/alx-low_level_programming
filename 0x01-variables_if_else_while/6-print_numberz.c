3e309234f1554494ab96f496558baf1c41ac93ae
DwayneEshun <dwayneeshun@gmail.com>
2023-02-18 06:05:16 +0000

    Numbers in style

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ * starting from `0`,
+ * followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
