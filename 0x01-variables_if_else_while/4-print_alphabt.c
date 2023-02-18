a661a5e2c2f56155402eb3cf0ab206cc2ffebd05
DwayneEshun <dwayneeshun@gmail.com>
2023-02-18 05:58:08 +0000

    Alphabt this time

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints the alphabets in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; (alphabet[i] != '\0'); i++)
+	{
+		if (((alphabet[i] != 'E') && (alphabet[i] != 'Q')))
+		{
+		putchar(tolower(alphabet[i]));
+		}
+		else
+		{													continue;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
