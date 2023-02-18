f0c09187e002185f9744f771d333a422b3a8612d
DwayneEshun <dwayneeshun@gmail.com>
2023-02-18 07:23:49 +0000

    Werror

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,13 +2,16 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
-/**
- * main - The boss itself
- *
- * Return: Returns Zero
- */
 
+/**
+ * main - Gets the last digit of
+ * a random number and
+ * prints whether its greater than,
+ * less than or
+ * equal to 0
+ *
+ * Return: 0 {Success}
+ */
 int main(void)
 {
 	int n;
@@ -29,13 +32,13 @@ int main(void)
 	}
 	else if ((lastDigit < 6) && (lastDigit != 0))
 	{
-		printf("Last digit of %d is %d
-				and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
+				, n, lastDigit);
 	}
 	else if ((lastDigit < 6) && (lastDigit == 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and is 0\n"
+				, n, lastDigit);
 	}
-
 	return (0);
 }
