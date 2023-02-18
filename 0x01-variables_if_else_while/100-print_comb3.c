92d9c35a1958716daf31685d45681ae8659cd6e0
DwayneEshun <dwayneeshun@gmail.com>
2023-02-18 06:51:52 +0000

    Pedantic

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all
+ * possible different combinations
+ * of two digits.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+
+	for (firstDigit = 0; firstDigit < 10; firstDigit++)
+	{
+		for (secondDigit = 0; secondDigit < 10; secondDigit++)
+		{
+			if (firstDigit < secondDigit)
+			{																		putchar(firstDigit + '0');
+			 putchar(secondDigit + '0');
+			}
+			else
+			{
+				continue;
+			}
+			if (!((firstDigit == 8) && (secondDigit == 9)))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
