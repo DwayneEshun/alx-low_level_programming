035aa3e1bbc295d7669697f727631eab3f49ade3
DwayneEshun <dwayneeshun@gmail.com>
2023-02-22 05:47:25 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - Prints '_putchar',
+ * followed by a new line.
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i;
+	char *aWord = "_putchar";
+
+	for (i = 0; aWord[i] != '\0'; i++)
+	{
+		_putchar(aWord[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
