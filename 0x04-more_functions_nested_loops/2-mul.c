5bf4372ec85c07cfe5b7b05204ae72c996118322
DwayneEshun <dwayneeshun@gmail.com>
2023-02-24 23:59:54 +0000

    Mully mully

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function definition
+ * Description: multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
