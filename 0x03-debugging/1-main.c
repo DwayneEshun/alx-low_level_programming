dddf6dd7e5a18c0dd6760e4746b954e11f8c5faf
DwayneEshun <dwayneeshun@gmail.com>
2023-02-25 05:54:56 +0000

    Debuggers Creed

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -the First Function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	* while (1 < 10)
+	* {
+	* putchar(i);
+	* }
+	*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
