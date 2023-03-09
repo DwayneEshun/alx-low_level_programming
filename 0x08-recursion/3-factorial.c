38916322b3e36d2515b3dd69a9a05135f943477b
DwayneEshun <dwayneeshun@gmail.com>
2023-03-09 05:15:22 +0000

    Powers In Hands of Man

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - Returns the factorial of a given number
+ * @n : Number to be used
+ * Return: Integer, Factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
+
