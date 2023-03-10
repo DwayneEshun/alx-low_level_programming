ff50318ae0cdcab26da652b20591382d682b5a72
DwayneEshun <dwayneeshun@gmail.com>
2023-03-10 22:35:59 +0000

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The Parent Function
+ * @argv: Arguement Vector
+ * @argc: Arguement Counter
+ * Return: My success story
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc >= 2)
+	{
+		int mul;
+
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+return (0);
+}
